da44f14c794ac1d885913f9045996af2f6588556
sbatchu <sbatchu@etouch.net>
2013-11-12 08:18:35 +0530

    initial commit

---
+++ git_local_repo/src/git/demo/HelloGit.java
@@ -0,0 +1,12 @@
+package git.demo;
+
+public class HelloGit {
+	
+	public static void main(String[] args){
+		System.out.println("--------------------------------------------------------");
+		System.out.println("------     Hello Welcome to Git World          ---------");
+		System.out.println("--------------------------------------------------------");
+
+	}
+
+}
